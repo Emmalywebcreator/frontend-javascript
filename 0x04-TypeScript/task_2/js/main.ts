c1201a796192e230c5cbeec9ac5adb5afd79fd93
Emmanuel Ohwoka <emmyprime2025@gmail.com>
2025-11-23 18:44:55 +0100

    Creates a function specific to employees

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,64 @@
+interface DirectorInterface {
+    workFromHome(): string;
+    getToWork(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return "Working from home";
+    }
+
+    getToWork(): string {
+        return "Getting a coffee break";
+    }
+
+    workDirectorTasks(): string {
+        return "Getting to director tasks";
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return "Cannot work from home";
+    }
+
+    getCoffeeBreak(): string {
+        return "Cannot have a break";
+    }
+
+    workTeacherTasks(): string {
+        return "Getting to work";
+    }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+    if (Number(salary) < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
+
+console.log(createEmployee(400)); // Teacher
+console.log(createEmployee(1000)); // Director
+
+function IsDirector(employee: Director | Teacher): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+    if (IsDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
+
+console.log(executeWork(createEmployee(200))); // Teacher tasks
+console.log(executeWork(createEmployee(1000))); // Director tasks
