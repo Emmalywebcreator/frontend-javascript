114b88900075b0b6516b0be253520d243af20d88
Emmanuel Ohwoka <emmyprime2025@gmail.com>
2025-11-25 19:41:19 +0100

    Change Subjects to Subject

--- 0x04-TypeScript/task_4/js/subjects/Java.ts
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -4,7 +4,7 @@ namespace Subjects {
     }
 
     export class Java extends Subject {
-        getRequirement(): string {
+        getRequirements(): string {
             return 'Here is the list of requirements for Java';
         }
 
