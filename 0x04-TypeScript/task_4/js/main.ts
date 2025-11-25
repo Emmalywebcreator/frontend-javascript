913b873bc7529a024d89526d4bc62c4c78bffcc5
Emmanuel Ohwoka <emmyprime2025@gmail.com>
2025-11-26 00:00:00 +0100

    main.js

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -3,7 +3,7 @@
 ///reference path="./Java.ts" />
 ///reference path="./React.ts" />   
 
-export const cpp = new Subjects.Cpp();
+export const cpp = new Cpp();
 export const java = new Subjects.Java();
 export const react = new Subjects.React();
 
