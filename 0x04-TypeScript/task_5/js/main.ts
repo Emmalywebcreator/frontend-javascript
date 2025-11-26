4bb18d23bada95bc5d53a8272987d35f9b1b1c61
Emmanuel Ohwoka <emmyprime2025@gmail.com>
2025-11-26 01:44:37 +0100

    Branding and Norminal typing

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,17 +1,29 @@
 export interface MajorCredits {
   credits: number;
-  __majorBrand: "major";
+  _majorBrand: "major";
 }
 
 export interface MinorCredits {
   credits: number;
-  __minorBrand: "minor";
+  _minorBrand: "minor";
 }
 
-export function sumMajorCredits(subjects1: MajorCredits, subjects2: MajorCredits): MajorCredits {
-    return { credits: subjects1.credits + subjects2.credits, __majorBrand: "major" };
+export function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits
+): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    _majorBrand: "major",
+  };
 }
 
-export function sumMinorCredits(subjects1: MinorCredits, subjects2: MinorCredits): MinorCredits {
-    return { credits: subjects1.credits + subjects2.credits, __minorBrand: "minor" };
+export function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits
+): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    _minorBrand: "minor",
+  };
 }
